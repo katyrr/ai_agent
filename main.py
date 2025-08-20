0446c0eccbb071e33ab6b59dc5f3d606f9537165
katyrr <krrobson9@gmail.com>
2025-08-20 10:23:51 +0100

    more setup

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+from google import genai
+client = genai.Client(api_key=api_key)
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
 def main():
     print("Hello from ai-agent!")
 
