0c749e33d181c3dedb03e67877e4d974b641831d
katyrr <krrobson9@gmail.com>
2025-08-20 10:57:41 +0100

    feat: add verbose flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,18 +15,36 @@ div = "--------------------------------------"
 def main():
     print("Hello from ai-agent!")
 
-    if len(argv) != 2:
-        print(f"wrong number of arguments: {len(argv)}")
+
+    if len(argv) <= 1:
+        print(f"missing argument: prompt")
         print(f"argv = {argv}")
         sys.exit(1)
 
     prompt = argv[1]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+
+    verbose = False
+    if "--verbose" in argv:
+        verbose = True
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    
+    
+    if verbose:
+        print(f"User prompt: {prompt}")
+        print(div)
+
     print(response.text)
     print(div)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(div)
     
 
 if __name__ == "__main__":
